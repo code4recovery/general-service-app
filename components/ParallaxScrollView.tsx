736b708059c0c6ec049818958c0c3324a3a7ec09
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2024-06-30 08:16:00 -0700

    pull to refresh

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,5 +1,10 @@
 import type { PropsWithChildren, ReactElement } from "react";
-import { StyleSheet, useColorScheme } from "react-native";
+import {
+  ActivityIndicator,
+  RefreshControl,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 import Animated, {
   interpolate,
   useAnimatedRef,
@@ -8,6 +13,7 @@ import Animated, {
 } from "react-native-reanimated";
 
 import { ThemedView } from "@/components/ThemedView";
+import { useContent } from "@/hooks/useContent";
 
 const HEADER_HEIGHT = 250;
 
@@ -24,6 +30,7 @@ export default function ParallaxScrollView({
   const colorScheme = useColorScheme() ?? "light";
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
+  const { loading, setLoading } = useContent();
 
   const headerAnimatedStyle = useAnimatedStyle(() => {
     return {
@@ -48,7 +55,16 @@ export default function ParallaxScrollView({
 
   return (
     <ThemedView style={styles.container}>
-      <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
+      <Animated.ScrollView
+        ref={scrollRef}
+        scrollEventThrottle={16}
+        refreshControl={
+          <RefreshControl
+            refreshing={loading}
+            onRefresh={() => setLoading(true)}
+          />
+        }
+      >
         <Animated.View
           style={[
             styles.header,
@@ -58,7 +74,11 @@ export default function ParallaxScrollView({
         >
           {headerImage}
         </Animated.View>
-        <ThemedView style={styles.content}>{children}</ThemedView>
+        {loading ? (
+          <ActivityIndicator size="large" style={styles.spinner} />
+        ) : (
+          <ThemedView style={styles.content}>{children}</ThemedView>
+        )}
       </Animated.ScrollView>
     </ThemedView>
   );
@@ -77,5 +97,9 @@ const styles = StyleSheet.create({
     padding: 24,
     gap: 16,
     overflow: "hidden",
+  },
+  spinner: {
+    marginVertical: 100,
+    marginHorizontal: "auto",
   },
 });
--- hooks/useContent.tsx
+++ hooks/useContent.tsx
@@ -8,21 +8,30 @@ import {
 
 import { type Entity } from "@/helpers/types";
 
-const ContentContext = createContext<{ entities?: Entity[] }>({
+const ContentContext = createContext<{
+  entities?: Entity[];
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+}>({
   entities: undefined,
+  loading: true,
+  setLoading: () => {},
 });
 
 export const ContentProvider = ({ children }: PropsWithChildren) => {
   const [entities, setEntities] = useState<Entity[]>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (!loading) return;
     fetch("https://generalservice.app/storage/6-6.json")
       .then((response) => response.json())
-      .then((entities) => setEntities(entities));
-  }, []);
+      .then((entities) => setEntities(entities))
+      .finally(() => setLoading(false));
+  }, [loading]);
 
   return (
-    <ContentContext.Provider value={{ entities }}>
+    <ContentContext.Provider value={{ entities, loading, setLoading }}>
       {children}
     </ContentContext.Provider>
   );
